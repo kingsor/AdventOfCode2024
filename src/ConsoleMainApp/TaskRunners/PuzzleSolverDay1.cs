8e9c82d12cbbb87336ad866cf996f0975a66e529
Edoardo Inamo <avventureweb@gmail.com>
2024-11-30 15:34:15 +0100

    Initial commit

---
+++ src/ConsoleMainApp/TaskRunners/PuzzleSolverDay1.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleMainApp.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleMainApp.TaskRunners;
+
+public class PuzzleSolverDay1 : IPuzzleSolver
+{
+    private readonly ILogger<PuzzleSolverDay1> _logger;
+
+    public PuzzleSolverDay1(ILogger<PuzzleSolverDay1> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Run(bool solveFirst = true)
+    {
+        if (solveFirst)
+        {
+            SolvePuzzleOne("./data/day1/test1.txt");
+
+            SolvePuzzleOne("./data/day1/input1.txt");
+        }
+
+        SolvePuzzleTwo("./data/day1/test2.txt");
+
+        SolvePuzzleTwo("./data/day1/input1.txt");
+    }
+
+    private void SolvePuzzleOne(string inputFile)
+    {
+        _logger.LogInformation("Solving puzzle one");
+
+        _logger.LogInformation($"Input file: {inputFile}");
+
+        if (File.Exists(inputFile))
+        {
+            // Solve puzzle
+        }
+        else
+        {
+            _logger.LogError("File not found");
+        }
+    }
+
+    private void SolvePuzzleTwo(string inputFile)
+    {
+        _logger.LogInformation("Solving puzzle two");
+
+        _logger.LogInformation($"Input file: {inputFile}");
+
+        if (File.Exists(inputFile))
+        {
+            // Solve Puzzle
+        }
+        else
+        {
+            _logger.LogError("File not found");
+        }
+    }
+}
---
+++ src/ConsoleMainApp/TaskRunners/PuzzleSolverDay2.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConsoleMainApp.TaskRunners
+{
+    public class PuzzleSolverDay2 : IPuzzleSolver
+    {
+        private readonly ILogger<PuzzleSolverDay2> _logger;
+
+        public PuzzleSolverDay2(ILogger<PuzzleSolverDay2> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Run(bool solveFirst = true)
+        {
+            if (solveFirst)
+            {
+                SolvePuzzleOne("./data/day2/test1.txt");
+
+                SolvePuzzleOne("./data/day2/input1.txt");
+            }
+
+            SolvePuzzleTwo("./data/day2/test2.txt");
+
+            SolvePuzzleTwo("./data/day2/input1.txt");
+        }
+
+        private void SolvePuzzleOne(string inputFile)
+        {
+            _logger.LogInformation("Solving puzzle one");
+
+            _logger.LogInformation($"Input file: {inputFile}");
+
+            if (File.Exists(inputFile))
+            {
+                // solve puzzle
+            }
+            else
+            {
+                _logger.LogError("File not found");
+            }
+        }
+
+        private void SolvePuzzleTwo(string inputFile)
+        {
+            _logger.LogInformation("Solving puzzle two");
+
+            _logger.LogInformation($"Input file: {inputFile}");
+
+            if (File.Exists(inputFile))
+            {
+                // solve puzzle
+            }
+            else
+            {
+                _logger.LogError("File not found");
+            }
+        }
+    }
+}
